5463e6745246ffc3c06be372f1bc5195092f8a38
faiziea <faizi@experienceamerica.com>
2022-06-29 04:59:59 +0500

    Mobile menu

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,9 +1,20 @@
 $(window).scroll(function() {    
     var scroll = $(window).scrollTop();
-    var header = $('.navbar').height();
-    if (scroll >= header) {
+    if (scroll > 400) {
         $("header").addClass("sticky_header");
     } else {
         $("header").removeClass("sticky_header");
     }
+});
+jQuery(document).ready(function() {
+    $(".navbar-toggler").click(function(){
+        $("body").addClass("overflow");
+    });
+    $(".menu_close, .nav-link").click(function(){
+        $("body").removeClass("overflow");
+    });
+    $(".menu_close, .nav-link").click(function(){
+        $("#collapsibleNavbar").removeClass("show");
+        $("#collapsibleNavbar").slideUp();
+    });
 });
